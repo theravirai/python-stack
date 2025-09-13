8c9b210b96c4c24b258138e336d754159e0efa96
ravirai <theravirai108@gmail.com>
2025-09-13 19:55:00 +0200

    implement disk usage monitoring

--- sys-watch/main.py
+++ sys-watch/main.py
@@ -1,4 +1,5 @@
 import psutil
+import shutil
 
 def get_cpu_usage():
     cpu_usage = psutil.cpu_percent(interval=1)
@@ -8,10 +9,19 @@ def get_memory_usage():
     memory = psutil.virtual_memory()
     return memory.percent
 
+def get_disk_usage():
+    disk = shutil.disk_usage("/")
+
+    used_percentage = (disk.used / disk.total) * 100
+
+    return round(used_percentage, 2)
+
 print("\n===== SYS WATCH =====\n")
 
 cpu = get_cpu_usage()
 memory = get_memory_usage()
+disk = get_disk_usage()
 
 print(f"CPU Usage: {cpu}%")
 print(f"Memory Usage: {memory}%")
+print(f"Disk Usage: {disk}%")
