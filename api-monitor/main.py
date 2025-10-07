9b7608d7a58bfee39ea78921ec6a843411dfa6d2
ravirai <theravirai108@gmail.com>
2025-10-07 22:13:00 +0200

    add api request error handling

--- api-monitor/main.py
+++ api-monitor/main.py
@@ -2,20 +2,29 @@ import requests
 import time
 
 def check_api(url):
-    start_time = time.time()
+    try:
+        start_time = time.time()
 
-    response = requests.get(url)
+        response = requests.get(url)
 
-    end_time = time.time()
+        end_time = time.time()
 
-    response_time = round(end_time - start_time, 3)
+        response_time = round(end_time - start_time, 3)
 
-    print("\n===== API MONITOR =====\n")
+        print("\n===== API MONITOR =====\n")
 
-    print(f"URL            : {url}")
-    print(f"Status Code    : {response.status_code}")
-    print(f"Response Time  : {response_time} seconds")
+        print(f"URL            : {url}")
+        print(f"Status Code    : {response.status_code}")
+        print(f"Response Time  : {response_time} seconds")
 
-    print("\n=======================\n")
+        print("\n=======================\n")
+
+    except requests.exceptions.RequestException as error:
+        print("\n===== API ERROR =====\n")
+
+        print(f"Failed to connect to: {url}")
+        print(f"Error: {error}")
+
+        print("\n=====================\n")
 
 check_api("https://api.github.com")
