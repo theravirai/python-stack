de8dda18099ae338c9be6e8b3824b3ea0a57173b
ravirai <theravirai108@gmail.com>
2025-10-07 18:12:00 +0200

    implement api monitoring log export

--- api-monitor/main.py
+++ api-monitor/main.py
@@ -1,5 +1,18 @@
 import requests
 import time
+import json
+
+from datetime import datetime
+
+def save_log(data):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    filename = f"logs/api_log_{timestamp}.json"
+
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
+    print(f"\nLog saved to: {filename}")
 
 def check_api(url):
     try:
@@ -11,6 +24,13 @@ def check_api(url):
 
         response_time = round(end_time - start_time, 3)
 
+        api_data = {
+            "url": url,
+            "status_code": response.status_code,
+            "response_time": response_time,
+            "checked_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+
         print("\n===== API MONITOR =====\n")
 
         print(f"URL            : {url}")
@@ -18,6 +38,8 @@ def check_api(url):
         print(f"Response Time  : {response_time} seconds")
 
         print("\n=======================\n")
+
+        save_log(api_data)
 
     except requests.exceptions.RequestException as error:
         print("\n===== API ERROR =====\n")
